5f428bd3dafc40c889d98d8844750ac80997cb35
Joao Gabriel <gama.gabrieljoao@gmail.com>
2025-05-20 19:10:00 -0300

    Metodo IPAdressToBits ajustado e metodo IPAdressToIpv4 feito

--- IPAdress.java
+++ IPAdress.java
@@ -19,7 +19,7 @@ public class IPAdress {
             tratados[i] = Integer.parseInt(partes[i]);
         }
         for (int i =0;i<tratados.length;i++){
-            binarios[i] = String.format("%8s",Integer.toBinaryString(tratados[i])).replace(" ", "0");
+            binarios[i] = String.format("%08s",Integer.toBinaryString(tratados[i]));
         }
         for (String i : binarios){
             sb.append(i);
@@ -30,6 +30,36 @@ public class IPAdress {
             formatado.append(resultado.charAt(i));
 
             if (i == 7 || i == 15 || i ==23){
+                formatado.append(".");
+            }
+        }
+
+        return formatado.toString();
+    }
+
+    public String IPAdressToIpv4(){
+        String [] partes = IPadress.split("\\.");
+        int [] decimal = new int[partes.length];
+        String [] tratar_para_string = new String[decimal.length];
+        StringBuilder sb = new StringBuilder();
+        StringBuilder formatado = new StringBuilder();
+        for (int i = 0;i<partes.length;i++){
+            decimal[i] = Integer.parseInt(partes[i],2);
+        }
+        for (int i =0;i<decimal.length;i++){
+            tratar_para_string[i] = String.format("%03d",decimal[i]);
+        }
+
+        for (String i : tratar_para_string){
+            sb.append(i);
+        }
+
+        String resultado = sb.toString();
+
+        for (int i = 0; i<resultado.length();i++){
+            formatado.append(resultado.charAt(i));
+
+            if (i == 2 || i == 5 || i == 8){
                 formatado.append(".");
             }
         }
